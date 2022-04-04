7b934e9f46d1675b31e567e9139a605badbf7ea6
Casey Corcoran <github@snaptopixel.com>
2022-04-04 17:23:55 -0400

    First commit

---
+++ src/demo/site.tsx
@@ -0,0 +1,36 @@
+import { css } from '@emotion/css'
+import { Component, FunctionalComponent, h } from '@stencil/core'
+import { JSXBase } from '@stencil/core/internal'
+
+// Just a flexbox w/centered content
+const CenteredBox: FunctionalComponent<JSXBase.IntrinsicElements['div']> = (
+  { class: className = '', ...attrs },
+  children
+) => {
+  return (
+    <div {...attrs} class={`${className} flex flex-col items-center justify-center`}>
+      {children}
+    </div>
+  )
+}
+
+// You can also still write css using emotion
+const customCSS = css`
+  font-family: Comic Sans MS;
+  color: orange;
+`
+
+@Component({
+  tag: 'demo-site',
+})
+export class Site {
+  render() {
+    return (
+      <CenteredBox class='h-screen w-screen'>
+        <h1 class='text-4xl font-black text-purple-700'>Hello World</h1>
+        <p class='italic text-purple-400'>Make changes anywhere and watch your browser update</p>
+        <span class={`${customCSS} font-bold uppercase`}>Much Wow</span>
+      </CenteredBox>
+    )
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{ts,tsx,html}'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
